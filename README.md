d59ab5b7a1e37e580c3d301c43d4dfffb59060e6
dmitrymartynov <domartynov@gmail.com>
2015-03-31 13:19:37 -0400

    Add .nuspec

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Autofac.Dispatcher
-Command dispatcher and Event pub/sub extension for Autofac
 
-[![Build status](https://ci.appveyor.com/api/projects/status/wra3g3ahgygteu5f?svg=true)
+Command dispatcher and Event pub/sub extension for Autofac.
+
+![Build status](https://ci.appveyor.com/api/projects/status/wra3g3ahgygteu5f?svg=true)
---
+++ test/Autofac.Dispatcher.Test/TypeUnifiedDispatchTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Xml;
+using Xunit;
+
+namespace Autofac.Dispatcher.Test
+{
+    public class TypeUnifiedDispatchTests
+    {
+        [Fact]
+        public void TestGenericHandlerDispatchedASpecializedCommand()
+        {
+            
+        }
+
+        public abstract class Entity<TKey>
+        {
+            public TKey Id { get; set; }
+        }
+
+        public class FooEntity : Entity<long>
+        {
+            
+        }
+
+        public class AllEntitiesLoaded<TEntity, TKey> : IEvent where TEntity : Entity<TKey>
+        {
+            public IEnumerable<TEntity> Entities { get; private set; }
+
+            public AllEntitiesLoaded(IEnumerable<TEntity> entities)
+            {
+                Entities = entities;
+            }
+        }
+
+        public class MemoryStore<TEntity, TKey> where TEntity : Entity<TKey>
+        {
+            private readonly TaskCompletionSource<ConcurrentDictionary<TKey, TEntity>> _initializeStoreTaskCompletionSource =  new TaskCompletionSource<ConcurrentDictionary<TKey, TEntity>>(); 
+            private readonly ConcurrentDictionary<TKey, TEntity> _entities = new ConcurrentDictionary<TKey, TEntity>();
+
+            public void Handle(AllEntitiesLoaded<TEntity, TKey> theEvent)
+            {
+                foreach (var entity in theEvent.Entities)
+                {
+                    _entities.TryAdd(entity.Id, entity);
+                }
+
+                _initializeStoreTaskCompletionSource.SetResult(_entities);
+            }
+        }
+
+
+    }
+}
